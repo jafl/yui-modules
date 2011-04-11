ac9e93ab424655fd56bba731efc66ba453c47b76
John Lindal <gitbug@newplanetsoftware.com>
2011-04-11 14:52:07 -0700

    quickedit example

---
+++ quickedit/yuidoc/classmap.js
@@ -0,0 +1,15 @@
+YAHOO.env.classMap = {"Column": "gallery-quickedit", "Y.Plugin.DataTableQuickEdit": "gallery-quickedit"};
+
+YAHOO.env.resolveClass = function(className) {
+    var a=className.split('.'), ns=YAHOO.env.classMap;
+
+    for (var i=0; i<a.length; i=i+1) {
+        if (ns[a[i]]) {
+            ns = ns[a[i]];
+        } else {
+            return null;
+        }
+    }
+
+    return ns;
+};
