e2cec62fef5f23764be70e549e563984a31d9014
John Lindal <gitbug@newplanetsoftware.com>
2011-06-02 16:46:17 -0700

    formmgr in an overlay

---
+++ formmgr-overlay-plugin/yuidoc/classmap.js
@@ -0,0 +1,15 @@
+YAHOO.env.classMap = {"Y.Plugin.OverlayForm": "gallery-formmgr-overlay-plugin"};
+
+YAHOO.env.resolveClass = function(className) {
+    var a=className.split('.'), ns=YAHOO.env.classMap;
+
+    for (var i=0; i<a.length; i=i+1) {
+        if (ns[a[i]]) {
+            ns = ns[a[i]];
+        } else {
+            return null;
+        }
+    }
+
+    return ns;
+};
