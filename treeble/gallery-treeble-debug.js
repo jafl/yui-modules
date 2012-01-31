f533a407d0c70e0da296e90b16e599e00924d3a1
John Lindal <lindalj@listalthough-lm.(none)>
2012-01-31 15:27:51 -0800

    update QuickEdit and Treeble to YUI 3.5.0 PR2

--- treeble/gallery-treeble-debug.js
+++ treeble/gallery-treeble-debug.js
@@ -1019,7 +1019,15 @@ Y.namespace("Parsers").treebledatasource = function(oData)
  *
  * @namespace
  * @class Treeble
+ * @constructor
+ * @param config {Object}
  */
+function Treeble()
+{
+	Treeble.superclass.constructor.apply(this, arguments);
+}
+
+Treeble.NAME = "datatable";		// same styling
 
 /**
  * <p>Formatter for open/close twistdown.</p>
@@ -1028,12 +1036,11 @@ Y.namespace("Parsers").treebledatasource = function(oData)
  * @param sendRequest {Function} Function that reloads DataTable
  * @static
  */
-Y.namespace("Treeble").buildTwistdownFormatter = function(sendRequest)
+Treeble.buildTwistdownFormatter = function(sendRequest)
 {
 	return function(o)
 	{
-		var td = this.createCell(o);
-		td.addClass('treeble-nub');
+		o.td.addClass('treeble-nub');
 
 		var ds  = this.datasource.get('datasource');
 		var key = ds.get('root').treeble_config.childNodesKey;
@@ -1044,22 +1051,18 @@ Y.namespace("Treeble").buildTwistdownFormatter = function(sendRequest)
 			var open  = ds.isOpen(path);
 			var clazz = open ? 'row-open' : 'row-closed';
 
-			td.addClass('row-toggle');
-			td.replaceClass(/row-(open|closed)/, clazz);
+			o.td.addClass('row-toggle');
+			o.td.replaceClass(/row-(open|closed)/, clazz);
 
-			td.on('click', function()
+			o.td.on('click', function()
 			{
 				ds.toggle(path, {}, sendRequest);
 			});
 
-			td.set('innerHTML', '<a class="treeble-collapse-nub" href="javascript:void(0);"></a>');
-		}
-		else
-		{
-			td.set('innerHTML', '');
+			o.cell.set('innerHTML', '<a class="treeble-collapse-nub" href="javascript:void(0);"></a>');
 		}
 
-		return '';
+		return true;	// keep the Y.Node instance
 	};
 };
 
@@ -1069,11 +1072,29 @@ Y.namespace("Treeble").buildTwistdownFormatter = function(sendRequest)
  * @method treeValueFormatter
  * @static
  */
-Y.namespace("Treeble").treeValueFormatter = function(o)
+Treeble.treeValueFormatter = function(o)
 {
 	var depth_class = 'treeble-depth-'+o.data._yui_node_depth;
 	return '<span class="'+depth_class+'">'+o.value+'</span>';
 };
 
+Y.extend(Treeble, Y.DataTable,
+{
+	plug: function(plugin, config)
+	{
+		if (plugin === Y.Plugin.DataTableDataSource)
+		{
+			var recordType = this.get('recordType');
+			recordType.ATTRS[ config.datasource.get('root').treeble_config.childNodesKey ] = {};
+			recordType.ATTRS._yui_node_path  = {};
+			recordType.ATTRS._yui_node_depth = {};
+		}
 
-}, '@VERSION@' ,{skinnable:true, requires:['datasource']});
+		Treeble.superclass.plug.apply(this, arguments);
+	}
+});
+
+Y.Treeble = Treeble;
+
+
+}, '@VERSION@' ,{requires:['datasource'], skinnable:true});
