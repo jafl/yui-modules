1ceeaac609e656310e2cdb033045935ba7f159fc
John Lindal <gitbug@newplanetsoftware.com>
2012-03-07 14:49:49 -0800

    merge from upstream

--- anim-class/gallery-funcprog-debug.js
+++ anim-class/gallery-funcprog-debug.js
@@ -185,4 +185,4 @@ Y.mix(Y,
 });
 
 
-}, '@VERSION@' ,{requires:['array-extras','gallery-object-extras'], optional:['gallery-nodelist-extras2']});
+}, '@VERSION@' ,{optional:['gallery-nodelist-extras2'], requires:['array-extras','gallery-object-extras']});
