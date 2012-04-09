277c45980bdd479770aa8e02ca98690ba3ef99c2
John Lindal <lindalj@listalthough-lm.(none)>
2012-04-08 20:02:15 -0700

    datatable-row-expansion:  patch getCell math:  add sign()

--- build/gallery-datatable-row-expansion/gallery-datatable-row-expansion-debug.js
+++ build/gallery-datatable-row-expansion/gallery-datatable-row-expansion-debug.js
@@ -6,7 +6,7 @@ YUI.add('gallery-datatable-row-expansion', function(Y) {
  * <p>Plugin for DataTable to show additional information for each row via
  * a twistdown.  The result of the template is displayed across all the
  * columns following the twistdown column.</p>
- * 
+ *
  * @module gallery-datatable-row-expansion
  * @namespace Plugin
  * @class RowExpansion
@@ -28,7 +28,7 @@ RowExpansion.ATTRS =
 {
 	/**
 	 * (Required) String template or function that returns a string.
-	 * 
+	 *
 	 * @config template
 	 * @type {String|Function}
 	 */
@@ -45,7 +45,7 @@ RowExpansion.ATTRS =
 	 * (Required) Id of a column (usually not displayed) that yields a
 	 * unique value for each record.  Used to maintain the twistdown state
 	 * when paginating.
-	 * 
+	 *
 	 * @config uniqueIdKey
 	 * @type {String}
 	 */
@@ -64,6 +64,15 @@ RowExpansion.ATTRS =
  * @value "row-expander"
  */
 RowExpansion.column_key = 'row-expander';
+
+/**
+ * The class added to rows created by this plugin.
+ *
+ * @property Y.RowExpansion.row_class
+ * @type {String}
+ * @value "row-expansion"
+ */
+RowExpansion.row_class = 'row-expansion';
 
 function formatTwistdown(o)
 {
@@ -111,7 +120,7 @@ function formatTwistdown(o)
 				'<td colspan="{post}" class="yui3-datatable-cell post-row-expansion">{tmpl}</td>' +
 			'</tr>',
 			{
-				c:    row.get('className') + ' row-expansion',
+				c:    row.get('className') + ' ' + RowExpansion.row_class,
 				pre:  pre_cells,
 				post: plugin.col_count.post,
 				tmpl: s
@@ -147,6 +156,101 @@ function analyzeColumns()
 		countColumns);
 }
 
+var shift_map =
+{
+	above:    [-1,  0],
+	below:    [ 1,  0],
+	next:     [ 0,  1],
+	prev:     [ 0, -1],
+	previous: [ 0, -1]
+};
+
+function getCell(seed, shift)
+{
+	var tbody = this.get('container'),
+		row, cell;
+
+	if (seed && tbody)
+	{
+		if (Y.Lang.isString(shift))
+		{
+			if (shift_map[shift])
+			{
+				shift = shift_map[shift];
+			}
+			else
+			{
+				throw Error('unknown shift in getCell: ' + shift);
+			}
+		}
+
+		if (Y.Lang.isArray(seed))
+		{
+			row  = tbody.get('children').item(0);
+			cell = row && row.get('children').item(seed[1]);
+			if (shift)
+			{
+				shift[0] += seed[0];
+			}
+			else
+			{
+				shift = [ seed[0], 0 ];
+			}
+		}
+		else if (seed._node)
+		{
+			cell = seed.ancestor('.' + this.getClassName('cell'), true);
+			if (cell.ancestor('tr.' + RowExpansion.row_class))
+			{
+				throw Error('getCell cannot be called with an element from an expansion row');
+			}
+		}
+
+		if (cell && shift)
+		{
+			var firstRowIndex = tbody.get('firstChild.rowIndex');
+			if (Y.Lang.isArray(shift))
+			{
+				row       = cell.ancestor();
+				var delta = Math.sign(shift[0]);
+				if (delta !== 0)
+				{
+					var rows  = tbody.get('children');
+					var index = row.get('rowIndex') - firstRowIndex;
+					var count = Math.abs(shift[0]);
+					for (var i=0; i<count && row; i++)
+					{
+						index += delta;
+						row    = rows.item(index);
+						if (row && row.hasClass(RowExpansion.row_class))
+						{
+							index += delta;
+							row    = rows.item(index);
+						}
+					}
+				}
+
+				index = cell.get('cellIndex') + shift[1];
+				cell  = row && row.get('children').item(index);
+			}
+		}
+	}
+
+	return (cell || null);
+}
+
+function replaceGetCell()
+{
+	var table = this.get('host');
+	if (!(table.body instanceof Y.DataTable.BodyView) ||
+		table.body.getCell === getCell)
+	{
+		return;
+	}
+
+	table.body.getCell = getCell;
+}
+
 Y.extend(RowExpansion, Y.Plugin.Base,
 {
 	initializer: function(config)
@@ -159,6 +263,8 @@ Y.extend(RowExpansion, Y.Plugin.Base,
 
 		analyzeColumns.call(this);
 		this.afterHostEvent('columnsChange', analyzeColumns);
+
+		this.afterHostEvent('renderTable', replaceGetCell);
 	}
 });
 
@@ -166,4 +272,4 @@ Y.namespace("Plugin");
 Y.Plugin.RowExpansion = RowExpansion;
 
 
-}, '@VERSION@' ,{requires:['datatable','plugin','gallery-funcprog']});
+}, '@VERSION@' ,{requires:['datatable','plugin','gallery-funcprog','gallery-node-optimizations','gallery-math']});
--- src/gallery-datatable-row-expansion/js/row-expansion.js
+++ src/gallery-datatable-row-expansion/js/row-expansion.js
@@ -4,7 +4,7 @@
  * <p>Plugin for DataTable to show additional information for each row via
  * a twistdown.  The result of the template is displayed across all the
  * columns following the twistdown column.</p>
- * 
+ *
  * @module gallery-datatable-row-expansion
  * @namespace Plugin
  * @class RowExpansion
@@ -26,7 +26,7 @@ RowExpansion.ATTRS =
 {
 	/**
 	 * (Required) String template or function that returns a string.
-	 * 
+	 *
 	 * @config template
 	 * @type {String|Function}
 	 */
@@ -43,7 +43,7 @@ RowExpansion.ATTRS =
 	 * (Required) Id of a column (usually not displayed) that yields a
 	 * unique value for each record.  Used to maintain the twistdown state
 	 * when paginating.
-	 * 
+	 *
 	 * @config uniqueIdKey
 	 * @type {String}
 	 */
@@ -62,6 +62,15 @@ RowExpansion.ATTRS =
  * @value "row-expander"
  */
 RowExpansion.column_key = 'row-expander';
+
+/**
+ * The class added to rows created by this plugin.
+ *
+ * @property Y.RowExpansion.row_class
+ * @type {String}
+ * @value "row-expansion"
+ */
+RowExpansion.row_class = 'row-expansion';
 
 function formatTwistdown(o)
 {
@@ -109,7 +118,7 @@ function formatTwistdown(o)
 				'<td colspan="{post}" class="yui3-datatable-cell post-row-expansion">{tmpl}</td>' +
 			'</tr>',
 			{
-				c:    row.get('className') + ' row-expansion',
+				c:    row.get('className') + ' ' + RowExpansion.row_class,
 				pre:  pre_cells,
 				post: plugin.col_count.post,
 				tmpl: s
@@ -145,6 +154,101 @@ function analyzeColumns()
 		countColumns);
 }
 
+var shift_map =
+{
+	above:    [-1,  0],
+	below:    [ 1,  0],
+	next:     [ 0,  1],
+	prev:     [ 0, -1],
+	previous: [ 0, -1]
+};
+
+function getCell(seed, shift)
+{
+	var tbody = this.get('container'),
+		row, cell;
+
+	if (seed && tbody)
+	{
+		if (Y.Lang.isString(shift))
+		{
+			if (shift_map[shift])
+			{
+				shift = shift_map[shift];
+			}
+			else
+			{
+				throw Error('unknown shift in getCell: ' + shift);
+			}
+		}
+
+		if (Y.Lang.isArray(seed))
+		{
+			row  = tbody.get('children').item(0);
+			cell = row && row.get('children').item(seed[1]);
+			if (shift)
+			{
+				shift[0] += seed[0];
+			}
+			else
+			{
+				shift = [ seed[0], 0 ];
+			}
+		}
+		else if (seed._node)
+		{
+			cell = seed.ancestor('.' + this.getClassName('cell'), true);
+			if (cell.ancestor('tr.' + RowExpansion.row_class))
+			{
+				throw Error('getCell cannot be called with an element from an expansion row');
+			}
+		}
+
+		if (cell && shift)
+		{
+			var firstRowIndex = tbody.get('firstChild.rowIndex');
+			if (Y.Lang.isArray(shift))
+			{
+				row       = cell.ancestor();
+				var delta = Math.sign(shift[0]);
+				if (delta !== 0)
+				{
+					var rows  = tbody.get('children');
+					var index = row.get('rowIndex') - firstRowIndex;
+					var count = Math.abs(shift[0]);
+					for (var i=0; i<count && row; i++)
+					{
+						index += delta;
+						row    = rows.item(index);
+						if (row && row.hasClass(RowExpansion.row_class))
+						{
+							index += delta;
+							row    = rows.item(index);
+						}
+					}
+				}
+
+				index = cell.get('cellIndex') + shift[1];
+				cell  = row && row.get('children').item(index);
+			}
+		}
+	}
+
+	return (cell || null);
+}
+
+function replaceGetCell()
+{
+	var table = this.get('host');
+	if (!(table.body instanceof Y.DataTable.BodyView) ||
+		table.body.getCell === getCell)
+	{
+		return;
+	}
+
+	table.body.getCell = getCell;
+}
+
 Y.extend(RowExpansion, Y.Plugin.Base,
 {
 	initializer: function(config)
@@ -157,6 +261,8 @@ Y.extend(RowExpansion, Y.Plugin.Base,
 
 		analyzeColumns.call(this);
 		this.afterHostEvent('columnsChange', analyzeColumns);
+
+		this.afterHostEvent('renderTable', replaceGetCell);
 	}
 });
 
