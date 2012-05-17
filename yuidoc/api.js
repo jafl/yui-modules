80aef527d88fad34a8d4e67980db65351cb5460a
John Lindal <lindalj@listalthough-lm.(none)>
2012-05-17 10:43:25 -0700

    add Y.ArrayIterator

--- yuidoc/api.js
+++ yuidoc/api.js
@@ -2,6 +2,7 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "Accordion",
+        "ArrayIterator",
         "ArrayList~extras",
         "Array~algorithms",
         "Array~funcprog-extras",
