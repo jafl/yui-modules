8a40cbc08841af694573e0663189a8c783f05036
John Lindal <github@newplanetsoftware.com>
2013-04-23 11:31:07 -0700

    multivalue-input: fix remove click bug, broadcast valuesChange event, fix yuidocs, add to Plugin namespace

--- src/gallery-multivalue-input/js/multivalue-input.js
+++ src/gallery-multivalue-input/js/multivalue-input.js
@@ -8,8 +8,9 @@
 
 /**
  *
- * @class QueryBuilder
- * @extends Widget
+ * @class MultivalueInput
+ * @namespace Plugin
+ * @extends Plugin.Base
  * @constructor
  * @param config {Object} Widget configuration
  */
@@ -19,24 +20,22 @@ function MultiValueInput(config) {
 
 
 //Any frequently used shortcuts
-var Lang   = Y.Lang, 
-MVI        = "multivalueinput", 
+var Lang   = Y.Lang,
+MVI        = "multivalueinput",
 getCN      = Y.ClassNameManager.getClassName,
-DIVCONTENT = getCN(MVI, "content"), 
-LIST       = getCN(MVI, "list"), 
-LIST_ITEM  = getCN(MVI,"listitem"), 
-UL         = getCN(MVI, "ul"), 
-INPUT      = getCN(MVI, "input"), 
+DIVCONTENT = getCN(MVI, "content"),
+LIST       = getCN(MVI, "list"),
+LIST_ITEM  = getCN(MVI, "listitem"),
+UL         = getCN(MVI, "ul"),
+INPUT      = getCN(MVI, "input"),
+REMOVE     = getCN(MVI, "remove"),
 Node       = Y.Node;
 
 
-
-
-
-/* 
- * Required NAME static field, to identify the Widget class and 
- * used as an event prefix, to generate class names etc. (set to the 
- * class name in camel case). 
+/*
+ * Required NAME static field, to identify the Widget class and
+ * used as an event prefix, to generate class names etc. (set to the
+ * class name in camel case).
  */
 MultiValueInput.NAME = "multiValueInput";
 
@@ -46,24 +45,24 @@ MultiValueInput.NS = "mvi";
  * The attribute configuration for the widget. This defines the core user facing state of the widget
  */
 MultiValueInput.ATTRS = {
-    /*
-     * Array of values selected
-     * @attribute values
-     * @type {array}
-     *
-     * */ 
+	/*
+	 * Array of values selected
+	 * @attribute values
+	 * @type {array}
+	 *
+	 * */
 	values : {
 		value : [],
 		validator: function(val) {
-	       return Lang.isArray(val);
-	    }
+		   return Lang.isArray(val);
+		}
 	},
-    /*
-     * Placeholder for inputbox
-     * @attribute placeholder
-     * @type {String}
-     *
-     * */ 
+	/*
+	 * Placeholder for inputbox
+	 * @attribute placeholder
+	 * @type {String}
+	 *
+	 * */
 	placeholder : {
 		value : "type here"
 	}
@@ -71,28 +70,24 @@ MultiValueInput.ATTRS = {
 
 /* Templates for any markup for multivalue input includes {} tokens, which are replaced through Y.substitute */
 
-MultiValueInput.DIV_TEMPLATE = "<div class={div_class}></div>";
+MultiValueInput.DIV_TEMPLATE = '<div class="{div_class}"></div>';
 
-//UL tag contain list of values 
-MultiValueInput.VALUE_HOLDER_TEMPLATE = "<ul class={ul_class}></ul>";
+//UL tag contain list of values
+MultiValueInput.VALUE_HOLDER_TEMPLATE = '<ul class="{ul_class}"></ul>';
 
-//LI tag contain item values 
-MultiValueInput.LI_TEMPLATE = "<li class={list_class}>{item}</li>";
+//LI tag contain item values
+MultiValueInput.LI_TEMPLATE = '<li class="{list_class}">{item}</li>';
 
-//markup for input control 
-MultiValueInput.INPUT_TEMPLATE = "<input type='text' placeholder={placeholder} class={input_class}></input>";
-
-//Markup for remove icon 
-MultiValueInput.ITEM_REMOVE = "<a href='javascript:void(0)'>,</a>";
+//Markup for remove icon
+MultiValueInput.ITEM_REMOVE = '<a href="javascript:void(0)" class="{remove_class}">,</a>';
 
 /* MultiValueInput extends the base Widget class */
 Y.extend(MultiValueInput, Y.Plugin.Base,
-		{
-
+{
 			initializer : function() {
-                var values = this.get("values"),placeholder=this.get("placeholder");
+				var values = this.get("values"),placeholder=this.get("placeholder");
 				this._host = this.get("host");
-				this._host.set("placeholder",placeholder); 
+				this._host.set("placeholder",placeholder);
 
 				//Create Container div that contains list of values
 				this._divContent = this._createDivNode();
@@ -100,18 +95,18 @@ Y.extend(MultiValueInput, Y.Plugin.Base,
 
 				//Create un order List
 				this._ul = this._createULNode();
-				
+
 				if (values) {
 					for(var i=0;i<values.length;i++){
-					    this._ul.appendChild(this._createListNode(values[i], true));
+						this._ul.appendChild(this._createListNode(values[i], true));
 					}
-	                Y.log("List Initialized:"+this.get("values"),"info","MultiValueInput.initializer");
+					Y.log("List Initialized:"+this.get("values"),"info","MultiValueInput.initializer");
 				}
 				this._host.addClass(INPUT);
 				this._inputWrapper = this._createListNode("");
 				this._inputWrapper.appendChild(this._host);
 				this._ul.appendChild(this._inputWrapper);
-				
+
 
 				this._divContent.appendChild(this._ul);
 
@@ -123,7 +118,7 @@ Y.extend(MultiValueInput, Y.Plugin.Base,
 				} else {
 					this._host.after("change", this._appendItem, this);
 				}
-				
+
 				this._host.after("blur", function() {
 					this._host.set("value", "");
 					this._divContent.removeClass("yui3-multivalueinput-border");
@@ -133,19 +128,21 @@ Y.extend(MultiValueInput, Y.Plugin.Base,
 					this._host.set("value", "");
 					this._divContent.addClass("yui3-multivalueinput-border");
 				},this);
-				
-				this._host.on("keydown", this._keyDownHandler, this);
-				
 
+				this._host.on("keydown", this._keyDownHandler, this);
+
+				this._divContent.delegate("click", function(e)
+				{
+					this._removeItem(e.currentTarget.ancestor("." + LIST_ITEM));
+				},
+				"." + REMOVE, this);
 			},
-			destructor : function() {
-			},
-			
+
 			/**
 			 * Generate Markup for UL tag
 			 * @method _keyDownHandler
 			 * @protected
-			 * @param {Event} 
+			 * @param {Event}
 			 */
 
 			_keyDownHandler:function(/* Event */ e){
@@ -163,25 +160,25 @@ Y.extend(MultiValueInput, Y.Plugin.Base,
 					}
 					else{
 						lastItem.addClass("yui3-multivalueinput-pendingdelete");
-  					    this._pendingDelete = true;
+						this._pendingDelete = true;
 					}
 				}
 			},
-			
+
 			/**
 			 * Generate Markup for UL tag
 			 * @method _createULNode
 			 * @protected
-			 * @return {Node} 
+			 * @return {Node}
 			 */
-			
+
 			_createULNode : function() {
 				return Node.create(Y.substitute(
 						MultiValueInput.VALUE_HOLDER_TEMPLATE, {
 							ul_class : UL
 						}));
 			},
-			
+
 			/**
 			 * Generate Markup for list tag
 			 * @method _createListNode
@@ -189,23 +186,23 @@ Y.extend(MultiValueInput, Y.Plugin.Base,
 			 * @param index {int} the section index
 			 * @return {Node}
 			 */
-			
+
 			_createListNode : function(
 					/* String */ item,
-					/* boolean */ isListItem) 
+					/* boolean */ isListItem)
 			{
 				var listNode, anchorNode;
 				if (isListItem) {
 					anchorNode = Node.create(Y
-							.substitute(MultiValueInput.ITEM_REMOVE));
+							.substitute(MultiValueInput.ITEM_REMOVE, {
+								remove_class : REMOVE
+							}));
 					listNode = Node.create(Y.substitute(
 							MultiValueInput.LI_TEMPLATE, {
 								item : item,
 								list_class : LIST
 							}));
 					listNode.appendChild(anchorNode);
-					anchorNode.on("click", Y.bind(this._removeItem, this,
-							listNode));
 					listNode.addClass(LIST_ITEM);
 				} else {
 					listNode = Node.create(Y.substitute(
@@ -216,20 +213,20 @@ Y.extend(MultiValueInput, Y.Plugin.Base,
 				}
 				return listNode;
 			},
-			
+
 			/**
 			 * Generate Markup for div tag
 			 * @method _createDivNode
 			 * @protected
-			 * @return {Node} 
+			 * @return {Node}
 			 */
-			
+
 			_createDivNode : function() {
 				return Node.create(Y.substitute(MultiValueInput.DIV_TEMPLATE, {
 					div_class : DIVCONTENT
 				}));
 			},
-			
+
 			/**
 			 * Get the index of remove item
 			 * @method _getListIndex
@@ -237,7 +234,7 @@ Y.extend(MultiValueInput, Y.Plugin.Base,
 			 * @param {Node}
 			 * @return {index}
 			 */
-			
+
 			_getListIndex : function(listNode) {
 				var allList=this._ul.get("children"),size=allList.size();
 				for(var i=0;i<size;i++){
@@ -247,12 +244,12 @@ Y.extend(MultiValueInput, Y.Plugin.Base,
 				}
 				return -1;
 			},
-			
+
 			/**
-			 * add the item in to the list 
+			 * add the item in to the list
 			 * @method _appendItem
 			 * @protected
-			 * @return 
+			 * @return
 			 */
 
 			_appendItem : function() {
@@ -261,15 +258,15 @@ Y.extend(MultiValueInput, Y.Plugin.Base,
 				this._addValue(val);
 				this._host.set("value", "");
 			},
-			
+
 			/**
-			 * remove the item from the list 
+			 * remove the item from the list
 			 * @method _removeItem
 			 * @protected
 			 * @param {Node}
-			 * @return 
+			 * @return
 			 */
-			
+
 			_removeItem : function(/* Node */selectedListNode, /* int */ idx) {
 				var index = idx || this._getListIndex(selectedListNode);
 				this._host.focus();
@@ -282,30 +279,32 @@ Y.extend(MultiValueInput, Y.Plugin.Base,
 			 * Add value to value list
 			 * @method _addValue
 			 * @protected
-			 * @return 
+			 * @return
 			 */
 
 			_addValue : function(val) {
 				var values = this.get("values");
 				values.push(val);
-                Y.log(val+" added to the List:"+this.get("values"),"info","MultiValueInput._addValue");
+				this.set("values", values.slice(0));	// force change event
+				Y.log(val+" added to the List:"+this.get("values"),"info","MultiValueInput._addValue");
 			},
-			
+
 			/**
 			 * remove value from value list
 			 * @method _removeValue
 			 * @protected
 			 * @param {index}
-			 * @return 
+			 * @return
 			 */
-			
+
 			_removeValue : function(/* int */ index) {
 				var values = this.get("values"), val = values[index];
 				values.splice(index,1);
-                Y.log(val+" removed from the List:"+this.get("values"),"info","MultiValueInput._removeValue");
+				this.set("values", values.slice(0));	// force change event
+				Y.log(val+" removed from the List:"+this.get("values"),"info","MultiValueInput._removeValue");
 			}
 
-			
-		});
+});
 
 Y.MultiValueInput = MultiValueInput;
+Y.namespace('Plugin').MultivalueInput = MultiValueInput;
