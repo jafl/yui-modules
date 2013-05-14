d6257b4a9f3172cc3b3c357c71c2dbb79d544830
John Lindal <github@newplanetsoftware.com>
2013-05-14 16:51:28 -0700

    update bulk editor examples

--- bulkedit/gallery-bulkedit-debug.js
+++ bulkedit/gallery-bulkedit-debug.js
@@ -1875,7 +1875,7 @@ Y.extend(BulkEditor, Y.Widget,
 				status[j] = 'error';
 				if (this.first_error_page == -1)
 				{
-					this.first_error_page = i;
+					this.first_error_page = j+1;
 				}
 			}
 		}
