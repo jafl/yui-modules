27170bbf8e31d0b9726a228fcbf0b95c761518a8
John Lindal <github@newplanetsoftware.com>
2014-12-23 19:26:52 -0800

    only delete key if it exists

--- src/gallery-querybuilder-daterange/js/daterange.js
+++ src/gallery-querybuilder-daterange/js/daterange.js
@@ -100,7 +100,10 @@ Y.QueryBuilder.DateRange.prototype =
 			var_config.start_config.DateTime.defaultDateTime = value[0];
 		}
 		this.start_widgets = this._attachDateTimeWidgets(this.start_id, var_config.start_config);
-		delete var_config.start_config.DateTime.defaultDateTime;
+		if (value[0])
+		{
+			delete var_config.start_config.DateTime.defaultDateTime;
+		}
 
 		if (value[1])
 		{
@@ -108,7 +111,10 @@ Y.QueryBuilder.DateRange.prototype =
 			var_config.end_config.DateTime.defaultDateTime = value[1];
 		}
 		this.end_widgets = this._attachDateTimeWidgets(this.end_id, var_config.end_config);
-		delete var_config.end_config.DateTime.defaultDateTime;
+		if (value[1])
+		{
+			delete var_config.end_config.DateTime.defaultDateTime;
+		}
 
 		this.range = new Y.DateTimeRange(
 		{
