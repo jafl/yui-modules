3a13d52550a3f90a353524944a4e23ae32357503
John Lindal <github@newplanetsoftware.com>
2014-12-30 20:32:53 -0800

    keep InputPopup DOM at (0,0) until first shown; prevents unwanted scrolling due to invisible div's sticking out below page content

--- src/gallery-popup/js/input-popup.js
+++ src/gallery-popup/js/input-popup.js
@@ -12,17 +12,6 @@
  */
 var InputPopup = function(config)
 {
-	if (Y.Lang.isUndefined(config.align))
-	{
-		config.align =
-		{
-			node:   config.inputField,
-			points: [ Y.WidgetPositionAlign.TL, Y.WidgetPositionAlign.BL ]
-		};
-
-		config.preventOverlap = true;
-	}
-
 	if (Y.Lang.isUndefined(config.constrain))
 	{
 		config.constrain = true;
@@ -48,7 +37,7 @@ var InputPopup = function(config)
 		}
 	];
 
-	InputPopup.superclass.constructor.apply(this, arguments);
+	InputPopup.superclass.constructor.call(this, config);
 };
 
 InputPopup.NAME = "inputpopup";
@@ -140,6 +129,22 @@ Y.extend(InputPopup, Y.Popup,
 			},
 			this);
 		});
+	},
+
+	show: function()
+	{
+		if (!this.get('align'))
+		{
+			this.set('align',
+			{
+				node:   this.get('inputField'),
+				points: [ Y.WidgetPositionAlign.TL, Y.WidgetPositionAlign.BL ]
+			});
+
+			this.set('preventOverlap', true);
+		}
+
+		InputPopup.superclass.show.apply(this, arguments);
 	}
 });
 
