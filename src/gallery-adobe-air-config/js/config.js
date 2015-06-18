fff1cf348dc7e4b2d9889b7bd77d794529571ac6
John Lindal <github@newplanetsoftware.com>
2015-06-17 19:23:25 -0700

    air-config & air-prefs

---
+++ src/gallery-adobe-air-config/js/config.js
@@ -0,0 +1,32 @@
+"use strict";
+
+Y.namespace('air');
+
+/**
+ * Implements read-only configuration data, loaded from `app:/config.json`.
+ *
+ * This is a singleton because there should only be one application configuration.
+ *
+ * @module gallery-adobe-air-config
+ */
+
+var file = air.File.applicationDirectory.resolvePath('conf.json'),
+	data = {};
+
+if (file.exists)
+{
+	var stream = new air.FileStream();
+	stream.open(file, air.FileMode.READ);
+	var s = stream.readUTFBytes(stream.bytesAvailable);
+	stream.close();
+
+	data = Y.JSON.parse(s);
+}
+
+Y.air.Config =
+{
+	get: function(key)
+	{
+		return data[ key ];
+	}
+};
---
+++ src/gallery-adobe-air-prefs/js/prefs.js
@@ -0,0 +1,51 @@
+"use strict";
+
+Y.namespace('air');
+
+/**
+ * Implements read-write preferences data, loaded from `app-storage:/prefs.json`.
+ *
+ * This is a singleton because there should only be one set of application preferences.
+ *
+ * @module gallery-adobe-air-prefs
+ */
+
+var file = air.File.applicationStorageDirectory.resolvePath('prefs.json'),
+	data = {};
+
+if (file.exists)
+{
+	load();
+}
+
+function load()
+{
+	var stream = new air.FileStream();
+	stream.open(file, air.FileMode.READ);
+	var s = stream.readUTFBytes(stream.bytesAvailable);
+	stream.close();
+
+	data = Y.JSON.parse(s);
+}
+
+function save()
+{
+	var stream = new air.FileStream();
+	stream.open(file, air.FileMode.WRITE);
+	var s = stream.writeUTFBytes(Y.JSON.stringify(data));
+	stream.close();
+}
+
+Y.air.Prefs =
+{
+	get: function(key)
+	{
+		return data[ key ];
+	},
+
+	set: function(key, value)
+	{
+		data[ key ] = value;
+		save();
+	}
+};
